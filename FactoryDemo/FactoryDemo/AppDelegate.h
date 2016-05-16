26b0ff9497853c2373fe1425eb4547fc337f07f7
limaofuyuanzhang <397524331@qq.com>
2016-05-16 18:01:07 +0800

    2016-05-16 company

---
+++ FactoryDemo/FactoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FactoryDemo
+//
+//  Created by bmob-LT on 16/5/11.
+//  Copyright © 2016年 bmob-LT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FactoryDemo/FactoryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FactoryDemo
+//
+//  Created by bmob-LT on 16/5/11.
+//  Copyright © 2016年 bmob-LT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
